32fbfcf3708a7906b6fa8d6fa773fc71d0c0b3f3
wowhdr <1252724+wowhdr@users.noreply.github.com>
2024-01-05 10:35:29 +0700

    fix user

--- User.php
+++ User.php
@@ -2,33 +2,40 @@
 
 class User {
 
-    $id;
-    $name;
-    $posts;
+    private $id;
+    private $name;
+    private $posts;
 
-    function create() {
+    function create($name) {
 
         // Create new user
 
         $this->name = $name;
+
+        $this->save();
     }
 
-    function update() {
+    function update($name) {
 
         // Update user
         $this->name = $name;
+
+        $this->save();
     }
 
     function save() {
 
         // Insert to database;
+
+        return "Success";
     }
 
     function posts() {
 
-        $post = new Post;
+        $post = new Post();
         
-
         $this->posts = $post->get_by_user($this->id);
+
+        return $this->posts;
     }
 }
