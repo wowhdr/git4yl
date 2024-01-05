188ec9a007b50cc2387eb9aa9e4cfb68ae86faae
wowhdr <1252724+wowhdr@users.noreply.github.com>
2024-01-05 10:18:57 +0700

    fix post class

--- Post.php
+++ Post.php
@@ -2,14 +2,27 @@
 
 class Post {
 
+    $title;
+    $body;
+    $author;
+    $tags;
+
     function create($user) {
 
         // Create new post
         // Post belongs to user
+
+        $author = $user;
     }
 
     function tags() {
 
         // Post has many tags
+        return $this->tags;
+    }
+
+    function save() {
+
+        // save post in database
     }
 }
