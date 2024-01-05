dc192e8d9a1121275d3685cedbbbcb885c52bbac
wowhdr <1252724+wowhdr@users.noreply.github.com>
2024-01-05 10:24:57 +0700

    fix user

--- User.php
+++ User.php
@@ -2,13 +2,33 @@
 
 class User {
 
+    $id;
+    $name;
+    $posts;
+
     function create() {
 
         // Create new user
+
+        $this->name = $name;
     }
 
     function update() {
 
         // Update user
+        $this->name = $name;
+    }
+
+    function save() {
+
+        // Insert to database;
+    }
+
+    function posts() {
+
+        $post = new Post;
+        
+
+        $this->posts = $post->get_by_user($this->id);
     }
 }
